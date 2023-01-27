58635c5c128a02e5e6a4adc0d11dd5c43f01d023
Jungseok Lee <jungsuk7083@gmail.com>
2023-01-27 17:39:26 +0900

    gggg

--- inflearn/src/section1/_1문자찾기.java
+++ inflearn/src/section1/_1문자찾기.java
@@ -7,7 +7,7 @@ class _1문자찾기{
 		t=Character.toUpperCase(t);
 		//System.out.println(str+" "+t);
 		/*for(int i=0; i<str.length(); i++){
-			if(str.charAt(i)==t) answer++;
+			if(str.charAt(i)==t) answer++; 
 		}*/
 		for(char x : str.toCharArray()){
 			if(x==t) answer++;
