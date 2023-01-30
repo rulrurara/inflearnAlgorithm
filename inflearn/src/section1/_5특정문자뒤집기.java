e631a4d436e5bafe01b6d3f240b0eacd47da6816
Jungseok Lee <jungsuk7083@gmail.com>
2023-01-30 17:58:35 +0900

    학원

---
+++ inflearn/src/section1/_5특정문자뒤집기.java
@@ -0,0 +1,28 @@
+package section1;
+
+import java.util.Scanner;
+
+public class _5특정문자뒤집기 {	
+	public static String solution(String str) {
+	String answer = "";
+	char[] qwe = str.toCharArray();
+	char[] a = new char[str.length()];
+	for(int i = 0 ; i < qwe.length;i++) {
+			a[i] = qwe[qwe.length -1 - i];
+	}
+	for(int i = 0; i<a.length;i++) {
+		answer += a[i];
+	}
+	//배열을 문자열로 바꾸기 이 방법 기억해두자!
+	return answer;
+}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		for(int i = 0 ; i < num ; i++) {
+			String str = sc.next();
+			System.out.println(solution(str));
+			System.out.println(str.getClass().getSimpleName());
+		}
+	}
+}
---
+++ inflearn/src/section1/_중복문자제거.java
@@ -0,0 +1,16 @@
+package section1;
+
+import java.util.Scanner;
+
+public class _중복문자제거 {
+
+	public static String solution(String str) {
+		String answer = "";
+		
+		return answer;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+	}
+}
